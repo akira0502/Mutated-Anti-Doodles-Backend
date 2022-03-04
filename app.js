cf25de88672717fe1b5bc79266c139362676db2b
dh_p8cn7d <dh_p8cn7d@iad1-shared-d12-01.dreamhost.com>
2022-03-04 05:46:42 -0800

    Run server with https

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+
+const fs = require('fs');
+var https = require('https');
+
+var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -11,7 +19,9 @@ app.get("/", (req, res) => {
     res.json({message: "Hello"});
 });
 require('./app/routes/MAD.routes.js')(app);
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT} .`);
+const PORT = process.env.PORT || 8433;
+
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(8443, () => {
+    console.log(`Server is running on port 8443 .`);
 });
